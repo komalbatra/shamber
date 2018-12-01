b9a4daed17c994dd5b8a29d4c288905239aea9fc
Sam <sbussey11@gmail.com>
2018-12-01 16:15:08 -0500

    adding db incl seed data

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,21 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS shamber_db;
+CREATE DATABASE shamber_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE shamber_db;
+
+CREATE TABLE share_a_shame (
+	id INT (10) AUTO_INCREMENT NOT NULL,
+    Name VARCHAR (100) NOT NULL,
+    Shame VARCHAR (100) NOT NULL,
+    Score INT (3)
+    PRIMARY KEY (id)
+);
+
+INSERT INTO share-a-shame (Name, Shame, Score)
+VALUES ("Beatrice", "Farted in church", 0);
+
+INSERT INTO share-a-shame (Name, Shame, Score)
+VALUES ("Jasper", "Robbed a nun", 0);
+
+INSERT INTO share-a-shame (Name, Shame, Score)
+VALUES ("Jasmine", "Stole candy from a kid", 0);
