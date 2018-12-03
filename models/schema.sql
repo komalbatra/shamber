9f0f9ded67dfbce0ee348a9ee7d06227adf041eb
Sam <sbussey11@gmail.com>
2018-12-03 07:23:22 -0500

    Correcting table name and separating seed data

--- models/schema.sql
+++ models/schema.sql
@@ -11,11 +11,11 @@ CREATE TABLE share_a_shame (
     PRIMARY KEY (id)
 );
 
-INSERT INTO share-a-shame (Name, Shame, Score)
+INSERT INTO share_a_shame (Name, Shame, Score)
 VALUES ("Beatrice", "Farted in church", 0);
 
-INSERT INTO share-a-shame (Name, Shame, Score)
+INSERT INTO share_a_shame (Name, Shame, Score)
 VALUES ("Jasper", "Robbed a nun", 0);
 
-INSERT INTO share-a-shame (Name, Shame, Score)
+INSERT INTO share_a_shame (Name, Shame, Score)
 VALUES ("Jasmine", "Stole candy from a kid", 0);
---
+++ models/seeds.sql
@@ -0,0 +1,27 @@
+-- Creating seed data for the shamber_db database --
+INSERT INTO share_a_shame (Name, Shame, Score)
+VALUES ("Beatrice", "Farted in church", 0);
+
+INSERT INTO share_a_shame (Name, Shame, Score)
+VALUES ("Jasper", "Robbed a nun", 0);
+
+INSERT INTO share_a_shame (Name, Shame, Score)
+VALUES ("Jasmine", "Stole candy from a kid", 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
