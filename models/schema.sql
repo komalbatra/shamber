b4a355084aed1142b49ba78d52646a88c4774927
Sam Bussey <sambussey@outlook.com>
2018-12-04 08:00:20 -0500

    Update schema.sql

--- models/schema.sql
+++ models/schema.sql
@@ -11,11 +11,3 @@ CREATE TABLE share_a_shame (
     PRIMARY KEY (id)
 );
 
-INSERT INTO share_a_shame (Name, Shame, Score)
-VALUES ("Beatrice", "Farted in church", 0);
-
-INSERT INTO share_a_shame (Name, Shame, Score)
-VALUES ("Jasper", "Robbed a nun", 0);
-
-INSERT INTO share_a_shame (Name, Shame, Score)
-VALUES ("Jasmine", "Stole candy from a kid", 0);
