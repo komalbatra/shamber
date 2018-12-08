b57ef29b1aff99b8034684eed2100742b217de0d
Rosyming <41979276+rosyming@users.noreply.github.com>
2018-12-08 11:58:40 -0500

    Re-cloning

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO share_a_shame (name, shame, score)
+INSERT INTO Shamber (name, shame, score)
 VALUES ("Beatrice", "Farted in church", 0);
 
-INSERT INTO share_a_shame (name, shame, score)
+INSERT INTO Shamber (name, shame, score)
 VALUES ("Jasper", "Robbed a nun", 0);
 
-INSERT INTO share_a_shame (name, shame, score)
+INSERT INTO Shamber (name, shame, score)
 VALUES ("Jasmine", "Stole candy from a kid", 0);
