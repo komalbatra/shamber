5f2c596848cdb98d8ed534720f6fa09824a19184
Rosyming <41979276+rosyming@users.noreply.github.com>
2018-12-08 12:26:43 -0500

    Changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,3 @@
-//API Routes
 // Requiring our Todo model
 var db = require("../models");
 
@@ -23,72 +22,72 @@ module.exports = function(app) {
 };
 
 /* *********************************************************************************
-  // apiRoutes.js - this file offers a set of routes for displaying and saving data to the db
-  // *********************************************************************************
-  
-  // Dependencies
-  // =============================================================
-  
-  // Requiring our models
-  var Shame = require("../models");
-  
-  // Routes
-  // =============================================================
-  module.exports = function(app) {
-    // Get all shames
-    app.get("/api/shames", function(req, res) {
-      // Finding all shames, and then returning them to the user as JSON.
-      Shame.Shamber.findAll({}).then(function(results) {
-        // results are available to us inside the .then
+// apiRoutes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var Shame = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // Get all shames
+  app.get("/api/shames", function(req, res) {
+    // Finding all shames, and then returning them to the user as JSON.
+    Shame.Shamber.findAll({}).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
+  });
+
+  // Add a shame
+  app.post("/api/shames", function(req, res) {
+    console.log("Shame Data:");
+    console.log(req.body);
+
+    Shame.Shamber.create({
+      name: req.body.name,
+      shame: req.body.shame,
+      createdAt: req.body.createdAt,
+      score: req.body.score
+    })
+      .then(function(results) {
+        // `results` here would be the newly created shame
         res.json(results);
-      });
-    });
-  
-    // Add a shame
-    app.post("/api/shames", function(req, res) {
-      console.log("Shame Data:");
-      console.log(req.body);
-  
-      Shame.Shamber.create({
-        name: req.body.name,
-        shame: req.body.shame,
-        createdAt: req.body.createdAt,
-        score: req.body.score
       })
-        .then(function(results) {
-          // `results` here would be the newly created shame
-          res.json(results);
-        })
-        .catch(function(err) {
-          console.error(err.messge, err);
-        });
-    });
-  };
-  
-  /* ORIGINAL ROUTES FROM HOMEWORK TEMPLATE
-  module.exports = function(app) {
-    // Get all examples
-    app.get("/api/examples", function(req, res) {
-      db.Example.findAll({}).then(function(dbExamples) {
-        res.json(dbExamples);
+      .catch(function(err) {
+        console.error(err.messge, err);
       });
+  });
+};
+
+/* ORIGINAL ROUTES FROM HOMEWORK TEMPLATE
+module.exports = function(app) {
+  // Get all examples
+  app.get("/api/examples", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.json(dbExamples);
     });
-  
-    // Create a new example
-    app.post("/api/examples", function(req, res) {
-      db.Example.create(req.body).then(function(dbExample) {
-        res.json(dbExample);
-      });
+  });
+
+  // Create a new example
+  app.post("/api/examples", function(req, res) {
+    db.Example.create(req.body).then(function(dbExample) {
+      res.json(dbExample);
     });
-  
-    // Delete an example by id
-    app.delete("/api/examples/:id", function(req, res) {
-      db.Example.destroy({
-        where: {
-          id: req.params.id
-        }
-      }).then(function(dbExample) {
-        res.json(dbExample);
-      });
+  });
+
+  // Delete an example by id
+  app.delete("/api/examples/:id", function(req, res) {
+    db.Example.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbExample) {
+      res.json(dbExample);
     });
-  }; */
+  });
+}; */
