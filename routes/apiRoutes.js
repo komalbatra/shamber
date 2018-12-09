69bf28d28b54d148f8a4ad8d092066fd1c1cd3e1
Rosyming <41979276+rosyming@users.noreply.github.com>
2018-12-09 15:27:07 -0500

    Updated API routes. Working code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,93 +1,93 @@
-// Requiring our Todo model
+// Requiring our Shamber model
 var db = require("../models");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
-  // POST route for saving a new todo
-  app.post("/api/shames", function(req, res) {
+  // POST route for saving a new shame
+  app.post("/api/shames/", function(req, res) {
     console.log(req.body);
-    // create takes an argument of an object describing the item we want to
-    // insert into our table. In this case we just we pass in an object with a text
-    // and complete property (req.body)
     db.Shamber.create({
       name: req.body.name,
       shame: req.body.shame,
       score: req.body.score
-    }).then(function(dbShamber) {
-      // We have access to the new todo as an argument inside of the callback function
-      res.json(dbShamber);
-    });
-  });
-};
-
-/* *********************************************************************************
-// apiRoutes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
-// Requiring our models
-var Shame = require("../models");
-
-// Routes
-// =============================================================
-module.exports = function(app) {
-  // Get all shames
-  app.get("/api/shames", function(req, res) {
-    // Finding all shames, and then returning them to the user as JSON.
-    Shame.Shamber.findAll({}).then(function(results) {
-      // results are available to us inside the .then
-      res.json(results);
-    });
-  });
-
-  // Add a shame
-  app.post("/api/shames", function(req, res) {
-    console.log("Shame Data:");
-    console.log(req.body);
-
-    Shame.Shamber.create({
-      name: req.body.name,
-      shame: req.body.shame,
-      createdAt: req.body.createdAt,
-      score: req.body.score
     })
-      .then(function(results) {
-        // `results` here would be the newly created shame
-        res.json(results);
+      .then(function(dbPost) {
+        res.json(dbPost);
+      })
+      .catch(function(err) {
+        console.error(err.messge, err);
+      });
+  });
+
+  // GET route for getting all of the shames
+  app.get("/api/shames/", function(req, res) {
+    db.Shamber.findAll({})
+      .then(function(dbPost) {
+        res.json(dbPost);
+      })
+      .catch(function(err) {
+        console.error(err.messge, err);
+      });
+  });
+
+  // GET route for retrieving a single shame
+  app.get("/api/shames/:id", function(req, res) {
+    db.Shamber.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      })
+      .catch(function(err) {
+        console.error(err.messge, err);
+      });
+  });
+
+  // DELETE route for deleting shames
+  app.delete("/api/shames/:id", function(req, res) {
+    db.Shamber.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      })
+      .catch(function(err) {
+        console.error(err.messge, err);
+      });
+  });
+
+  // PUT route for updating shames
+  app.put("/api/shames/", function(req, res) {
+    db.Shamber.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      })
+      .catch(function(err) {
+        console.error(err.messge, err);
+      });
+  });
+
+  // GET route for returning shames of a specific category (e.g., top shames, newest shames)
+  app.get("/api/shames/category/:category", function(req, res) {
+    db.Shamber.findAll({
+      where: {
+        category: req.params.category
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
       })
       .catch(function(err) {
         console.error(err.messge, err);
       });
   });
 };
-
-/* ORIGINAL ROUTES FROM HOMEWORK TEMPLATE
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-}; */
