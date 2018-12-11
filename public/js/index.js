d2226b2fae0a3decd95dac1f710b374654662ba7
Komal Batra <KomalBatra@gmail.com>
2018-12-10 22:43:27 -0500

    JS updated and Unit Tests added

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   var $nameText = $("#user_name");
   var $shameText = $("#user_shame");
+  var $shameContainer = $("#shame-list"); //(Mary add)
+  var shames; //(Mary add)
+
+  getShames();
 
   //Function for capturing user input
   function insertShame(event) {
@@ -31,6 +35,39 @@ $(document).ready(function() {
       return;
     }
   }
+  //Mary's Code
+  function getShames() {
+    $.get("/api/shames", function(data) {
+      console.log("Shames", data);
+      shames = data;
+      if (!shames || !shames.length) {
+        displayEmpty();
+      } else {
+        initializeRows();
+      }
+    });
+  }
+  // Function for appending shames inside shame container (MARY add)
+  function initializeRows() {
+    $shameContainer.empty();
+    var shamesToAdd = [];
+    for (var i = 0; i < shames.length; i++) {
+      console.log(shamesToAdd);
+      //shamesToAdd.push(createNewRow(shames[i]));
+    }
+    $shameContainer.append(shamesToAdd);
+  }
+  // This function displays a message when there are no shames (MARY add)
+  function displayEmpty() {
+    $shameContainer.empty();
+    var messageH2 = $("<h2>");
+    messageH2.css({ "text-align": "center", "margin-top": "50px" });
+    messageH2.html(
+      "No shames yet for this category, navigate <a href='/'>here</a> in order to create a new shame."
+    );
+    $shameContainer.append(messageH2);
+  }
+  //__________________
   //Event listener
   $(document).on("submit", insertShame);
 });
