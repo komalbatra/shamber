189506789bf9433fb2a54ef7614c8d889c24a1f7
Rosyming <41979276+rosyming@users.noreply.github.com>
2018-12-11 09:21:42 -0500

    Made some changes to html route and cleaned up some code

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,49 +1,30 @@
+// Requiring our Shamber model
+//var db = require("../models");
+
 // HTML Routes
 // =============================================================
 module.exports = function(app) {
   // Route to the shareashame handlebar view
   app.get("/", function(req, res) {
-    res.render("shareashame", {
-      msg: "Welcome Mary!"
-    });
+    res.render("shareashame");
   });
 
-  // Route to the wall of shame handlebar view
   app.get("/wall", function(req, res) {
     res.render("wallofshame");
   });
+
+  /* Route to the wall of shame handlebar view
+  app.get("/wall", function(req, res) {
+    db.Shamber.findAll({}).then(function(dbPost) {
+      res.render("wallofshame", {
+        msg: "",
+        shamePosts: dbPost
+      });
+    });
+  });*/
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
 };
-
-/* ORIGINAL ROUTES FROM HOMEWORK TEMPLATE
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-}; */
